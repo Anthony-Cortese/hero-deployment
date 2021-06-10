71de3665fe9ff3233e1064420b35c4c5d61c01b6
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2021-06-10 10:08:09 -0600

    built out index.js and react app is built

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("its alive");
+console.log(__dirname);
+console.log(__filename);
+console.log(process.env.USER);
+console.log(process.env.PORT);
+
+// const dotenv = require("dotenv").congig();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 9000;
+const path = require("path");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.use("/api", (_, res) => {
+  res.json({ data: "Your API has data working!!" });
+});
+
+app.listen(port, () => {
+  console.log(`server is working on port ${port}`);
+});
+
+app.use("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
