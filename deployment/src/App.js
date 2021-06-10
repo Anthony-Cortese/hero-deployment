c7ac0f8cbba25628dc292cc8f492f3b5c3a0ea1d
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2021-06-10 10:31:08 -0600

    built a quick app

--- deployment/src/App.js
+++ deployment/src/App.js
@@ -1,22 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function url(path) {
+  return process.env.NODE_ENV === "development"
+    ? `http://localhost:9000${path}`
+    : path;
+}
 
 function App() {
-  return <div></div>;
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    fetch(url("/api"))
+      .then((res) => res.json())
+      .then((apiData) => setData(apiData.data));
+  }, []);
+
+  return <div> API data returned: {data}</div>;
 }
 
 export default App;
-
-// function url(path) {
-//   return process.env.NODE_ENV === "development"
-//     ? `http://localhost:9000${path}`
-//     : path;
-// }
-
-// function App() {
-//   const [data, setData] = useState("");
-
-//   useEffect(() => {
-//     fetch(url("/api"))
-//       .then((res) => res.json())
-//       .then((apiData) => setData(apiData.data));
-//   }, []);
